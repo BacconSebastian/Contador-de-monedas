a9cc21f4decaa8bdafe9d195c94bf5597265e583
Sebastian <bacconsebastian@gmail.com>
2021-05-26 00:39:34 -0300

    Update contador.py

--- contador.py
+++ contador.py
@@ -2,8 +2,8 @@ import cv2 as cv2
 import numpy as np
 
 # Estos valores deben cambiarse segun la imagen a estudiar.
-vg = 9
-vk = 33
+vg = 1
+vk = 3
 
 original = cv2.imread('Imagenes/monedassoles.jpg')
 gris = cv2.cvtColor(original,cv2.COLOR_BGR2GRAY)
@@ -11,8 +11,7 @@ gauss = cv2.GaussianBlur(gris, (vg,vg), 0)
 canny = cv2.Canny(gauss, 60, 100)
 kernel = np.ones( (vk,vk) , np.uint8 )
 cierre = cv2.morphologyEx(canny, cv2.MORPH_CLOSE, kernel)
-apertura = cv2.morphologyEx (cierre, cv2.MORPH_OPEN, kernel)
-contorno, jerarquia = cv2.findContours(apertura.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+contorno, jerarquia = cv2.findContours(cierre.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 
 print("Monedas encontradas: {}".format(len(contorno)))
